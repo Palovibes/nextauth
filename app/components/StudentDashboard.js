2724b81f01decce40869fbdc1f256276517510ed
FULL_NAME <EMAIL_ADDRESS>
2024-03-28 21:20:37 -0700

    created studentDashboard basic

---
+++ app/components/StudentDashboard.js
@@ -0,0 +1,12 @@
+// app/components/StudentDashboard.js
+const StudentDashboard = () => {
+    return (
+      <div>
+        <h1>Student Dashboard</h1>
+        <p>Welcome, student! This is your dashboard.</p>
+      </div>
+    );
+  };
+  
+  export default StudentDashboard;
+  
