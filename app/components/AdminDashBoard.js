b4884ecd6407f830b62b2b1bfa28d6d5b39eb281
FULL_NAME <EMAIL_ADDRESS>
2024-03-28 21:24:59 -0700

    created Admin Dashboard

---
+++ app/components/AdminDashBoard.js
@@ -0,0 +1,12 @@
+// app/components/StudentDashboard.js
+const StudentDashboard = () => {
+    return (
+      <div>
+        <h1>Admin Dashboard</h1>
+        <p>Welcome, Admin! This is your dashboard.</p>
+      </div>
+    );
+  };
+  
+  export default AdminDashboard;
+  
